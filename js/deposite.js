f76ac9640c7d3fea29bfccf015bb91955073fe90
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2022-08-23 09:32:32 +0600

    withdraw baki ase

--- js/deposite.js
+++ js/deposite.js
@@ -3,13 +3,35 @@
 document.getElementById('deposite-btn').addEventListener('click',function name() {
     //step:2 connect the deposite input filed 
     const depositeFiled = document.getElementById('deposite-input');
-    const depositeAmount =  depositeFiled.value;
+    const newdepositeAmountString =  depositeFiled.value;
+    const newdepositeAmount = parseFloat(newdepositeAmountString);
+    // for input clear 
+    depositeFiled.value = '';
     
     //step: 3 get the current deposite total
 
     const depositetotalElement = document.getElementById('depositeTotal');
-    const depositetotal = depositetotalElement.innerText;
-    depositetotalElement.innerText = depositeAmount;
+    const previsedepositetotalString = depositetotalElement.innerText;
+    //string to float
+    const previseDeposite = parseFloat(previsedepositetotalString);
+
+    // 2 ta man add kori
+     const currentdepositeAmount = newdepositeAmount + previseDeposite;
+    depositetotalElement.innerText = currentdepositeAmount;
+
+
+
+    // total balance ke niya asi
+
+    const balanceTotalElement  = document.getElementById('totalBalance');
+    const previsebalanceTotalElementString = balanceTotalElement.innerText;
+    const privisebalanceTotal = parseFloat(previsebalanceTotalElementString);
+
+
+    const currentTotalBalance = newdepositeAmount + privisebalanceTotal;
+    balanceTotalElement.innerText = currentTotalBalance;
+
+    
 
     
 })
